a431f1e99c4eb2907dc8d996e47ddc6404d4f731
Aşkın Gedik <askn@bil.omu.edu.tr>
2016-08-07 16:38:21 +0300

    add layout

--- src/racon.cr
+++ src/racon.cr
@@ -1,5 +1,20 @@
 require "./racon/*"
 
-module Racon
-  # TODO Put your code here
+require "kemal"
+require "pg"
+require "pool/connection"
+
+require "./views/*"
+
+serve_static false
+add_handler Kemal::StaticFileHandler.new "./public"
+
+add_handler Kemal::StaticFileHandler.new "./node_modules/jquery/dist"
+add_handler Kemal::StaticFileHandler.new "./node_modules/what-input"
+add_handler Kemal::StaticFileHandler.new "./node_modules/foundation-sites/dist"
+
+get "/" do
+  render "src/views/index.html.ecr", "src/views/layout.html.ecr"
 end
+
+Kemal.run
